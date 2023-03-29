b55bd042eb31e4b4bc44187e27662223cf235145
Paulo Geyer <paulogeyer@gmail.com>
2023-03-29 20:51:05 -0300

    okay

--- crypto/xor/otr.js
+++ crypto/xor/otr.js
@@ -9,33 +9,52 @@ class OTR {
     return bytes.map(x => String.fromCharCode(x)).join('');
   }
 
+  static bytes_to_hex(bytes) {
+    var res = [];
+
+    for(var i = 0; i < bytes.length; i++) {
+      res.push(bytes[i].toString(16));
+    }
+
+    return res.join('');
+  }
+
   static hex_to_bytes(h) {
     var res = [];
 
     for(var i = 0; i < h.length/2; i++) {
-      res.push(parseInt(h.split(i,i+2), 16));
+      res.push(parseInt(h.slice(i,i+2), 16));
     }
 
     return res;
   }
 
-  static xor(k,m) {
+  static xor(k,m,repeat=false) {
+    var k_length = k.length;
+    var m_length = m.length;
+
+    if(!repeat && (k_length < m_length)) {
+      throw "key is smaller than the message";
+    }
+
     var res = [];
 
     for(var i = 0; i < m.length; i++) {
-      res.push(k[i]^m[i]);
+      res.push(k[(i % k_length)] ^ m[i]);
     }
     return res;
   }    
 
-  static encode(k,m) {
-    return this.xor(k,m);
+  static encode(k,m,repeat=false) {
+    return this.xor(k,m,repeat);
   }
 
-  static decode(k,c) {
-    return this.xor(k,c);
+  static decode(k,c,repeat=false) {
+    return this.xor(k,c,repeat);
   }
 }
+
+c1 = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104";
 
 s1 = "foo bar";
 s1_bytes = OTR.str_to_bytes(s1);
@@ -44,7 +63,17 @@ k_bytes = OTR.str_to_bytes(k);
 
 // console.log(OTR.encode(k, s1));
 
-console.log("s1: "+s1);
-console.log(s1_bytes);
-console.log(k_bytes);
-console.log(OTR.bytes_to_str(OTR.decode(k_bytes, OTR.encode(k_bytes,s1_bytes))));
+// console.log("s1: "+s1);
+// console.log(s1_bytes);
+// console.log(k_bytes);
+// console.log(OTR.bytes_to_str(OTR.decode(k_bytes, OTR.encode(k_bytes,s1_bytes))));
+
+console.log(OTR.hex_to_bytes(c1));
+cypher = OTR.encode(OTR.str_to_bytes("secret"),
+		    OTR.str_to_bytes("my test message"), true)
+
+console.log(cypher);
+console.log(OTR.bytes_to_hex(cypher));
+
+text = OTR.decode(OTR.str_to_bytes("secret"), cypher, true);
+console.log(OTR.bytes_to_str(text));

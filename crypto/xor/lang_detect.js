daa080feca519fc0411cfc438b9e9d135c57c10c
Paulo Geyer <paulogeyer@gmail.com>
2023-03-30 04:14:11 -0300

    language detection utils

--- crypto/xor/lang_detect.js
+++ crypto/xor/lang_detect.js
@@ -1,7 +1,46 @@
 
+const fs = require('fs');
+
 class LangDetect {
-  static loadDictionary() {
-    return null;
+  static load_dictionary() {
+    fs.readFile(fname, "utf8", (err, data) => {
+      var lines = data.split("\n");
+      var res = {};
+
+      for(var i = 0; i < lines.length; i++) {
+	res[lines[i]] = null;
+      }
+    });
+
+    return res;
+  }
+
+  static get_english_count(message) {
+    var m = this.remove_non_letters(message.toUpperCase());
+    var possible_words = m.split(' ');
+
+    if (possible_words.length == 0)
+      return 0.0;
+
+    var matches = 0;
+
+    for(var i = 0; i < possible_words.length; i++) {
+      if(ENGLISH_WORDS.includes(possible_words[i]))
+	matches++;
+    }
+    
+    return 1.0*matches/possible_words.length;
+  }
+
+  static remove_non_letters(msg) {
+    var letters_only = [];
+
+    for(var i = 0; i < msg.length; i++) {
+      if(LETTERS_AND_SPACE.includes(msg[i]))
+	letters_only.push(msg[i]);
+    }
+
+    return letters_only.join('');
   }
 
   static isEnglish(message, word_percentage=20, letter_percentage=85) {

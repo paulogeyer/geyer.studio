1ed0c73bcec1057c130ec99e1290353de193f5b0
Paulo Geyer <paulogeyer@gmail.com>
2023-03-30 04:24:04 -0300

    class definition fixes

--- crypto/xor/lang_detect.js
+++ crypto/xor/lang_detect.js
@@ -2,7 +2,16 @@
 const fs = require('fs');
 
 class LangDetect {
-  static load_dictionary() {
+  constructor(msg, word_percentage=20, letter_percentage=85) {
+    this.msg = msg;
+    this.word_percentage = word_percentage;
+    this.letter_percentage = letter_percentage;
+    this.UPPER_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.LETTERS_AND_SPACE = UPPER_LETTERS+UPPER_LETTERS.toLowerCase()+'\t\n';
+    this.ENGLISH_WORDS = this.load_dictionary();
+  }
+
+  load_dictionary() {
     fs.readFile(fname, "utf8", (err, data) => {
       var lines = data.split("\n");
       var res = {};
@@ -15,8 +24,8 @@ class LangDetect {
     return res;
   }
 
-  static get_english_count(message) {
-    var m = this.remove_non_letters(message.toUpperCase());
+  static get_english_count() {
+    var m = this.remove_non_letters(this.msg.toUpperCase());
     var possible_words = m.split(' ');
 
     if (possible_words.length == 0)
@@ -43,11 +52,11 @@ class LangDetect {
     return letters_only.join('');
   }
 
-  static isEnglish(message, word_percentage=20, letter_percentage=85) {
-    var words_match = get_english_count(message) * 100 >= word_percentage;
-    var num_letters = remove_non_letters(message).split('').length;
-    var message_letters_percentage = num_letters / message.length * 100;
-    var letters_match = message_letters_percentage >= letter_percentage;
+  static is_english() {
+    var words_match = get_english_count(this.msg) * 100 >= this.word_percentage;
+    var num_letters = remove_non_letters(this.msg).split('').length;
+    var message_letters_percentage = num_letters / this.msg.length * 100;
+    var letters_match = message_letters_percentage >= this.letter_percentage;
 
     return words_match && letters_match;
   }
